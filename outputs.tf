00afa81f23657bbfc378a660cf8a97878b409a20
Rita Sokolova <msokolova13@gmail.com>
2021-03-15 12:59:06 -0400

    Initial configuration

---
+++ outputs.tf
@@ -0,0 +1,18 @@
+output "rds_hostname" {
+  description = "RDS instance hostname"
+  value       = aws_db_instance.education.address
+  sensitive   = true
+}
+
+output "rds_port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.education.port
+  sensitive   = true
+}
+
+output "rds_username" {
+  description = "RDS instance root username"
+  value       = aws_db_instance.education.username
+  sensitive   = true
+}
+
